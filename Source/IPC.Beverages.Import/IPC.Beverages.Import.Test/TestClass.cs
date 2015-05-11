cf7254c3192516b51ce1b85b3171df36a14e6236
IPC\nperez <nperez@ipcoop.com>
2015-05-11 16:10:01 -0400

    Initial Commit

---
+++ Source/IPC.Beverages.Import/IPC.Beverages.Import.Test/TestClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+
+namespace IPC.Beverages.Import.Test
+{
+
+    struct MyStruct
+    {
+        public int a;
+
+        public void X()
+        {
+        }
+    }
+
+    public class TestClass 
+    {
+        [Fact]
+        public void SomeTrueTest()
+        {
+            true.Should().BeTrue();
+        }
+
+        [Fact]
+        public void SomeFalseTest()
+        {
+            false.Should().BeFalse();
+            int[] x = new int[10];
+
+            var s = new List<string>();
+            
+            
+        }
+    }
+}
---
+++ Source/IPC.Beverages.Import/IPC.Beverages.Import/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPC.Beverages.Import
+{
+    public class Class1
+    {
+    }
+
+
+}
